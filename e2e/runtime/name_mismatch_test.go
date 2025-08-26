47965254b83acbe7d45cb17f821b3dca8e026372
Bartek Mucha <bartosz.mucha@arm.com>
2025-08-26 10:56:49 +0100

    test: Sample e2e failure test case for runtime (#16)

---
+++ e2e/runtime/name_mismatch_test.go
@@ -0,0 +1,30 @@
+package runtime
+
+import (
+	"testing"
+
+	"github.com/Arm-Debug/remoteproc-runtime/e2e/shared"
+	"github.com/Arm-Debug/remoteproc-simulator/pkg/simulator"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRemoteprocNameMismatch(t *testing.T) {
+	simConfig := simulator.Config{
+		RootDir: t.TempDir(),
+		Index:   1,
+		Name:    "some-processor",
+	}
+	sim, err := simulator.NewRemoteproc(simConfig)
+	if err != nil {
+		t.Fatalf("failed to run simulator: %s", err)
+	}
+	defer sim.Close()
+	bin, err := buildRuntimeBinary(t.TempDir(), simConfig.RootDir)
+	require.NoError(t, err)
+
+	bundlePath := t.TempDir()
+	require.NoError(t, shared.GenerateBundle(bundlePath, "other-processor"))
+	_, err = invokeRuntime(bin, "create", "--bundle", bundlePath, "test-container")
+	assert.ErrorContains(t, err, "other-processor is not in the list of available remote processors")
+}
