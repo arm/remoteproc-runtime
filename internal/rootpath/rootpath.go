af25f45401037ce227f39250fd7c75440cd9c9e8
Luke Parkin <65398091+Luke-Parkin@users.noreply.github.com>
2025-09-02 15:10:52 +0100

    fix: Handle tilde expansion (#25)
    Signed-off-by: luke <luke.parkin@arm.com>

--- internal/rootpath/rootpath.go
+++ internal/rootpath/rootpath.go
@@ -1,9 +1,39 @@
 package rootpath
 
-import "path/filepath"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 // Use `-X internal.rootpath.prefix=/some/path` at build time to override
 var prefix string = "/"
+
+func ExpandTilde(path string) (string, error) {
+	if strings.HasPrefix(path, "~") {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return path, fmt.Errorf("can't expand ~: %w", err)
+		}
+
+		if path == "~" {
+			path = home
+		}
+		if strings.HasPrefix(path, "~/") {
+			path = filepath.Join(home, path[2:])
+		}
+	}
+	return path, nil
+}
+
+func init() {
+	var err error
+	prefix, err = ExpandTilde(prefix)
+	if err != nil {
+		panic("failed to get absolute path of prefix: " + err.Error())
+	}
+}
 
 func Join(segments ...string) string {
 	if len(segments) == 0 {
--- internal/rootpath/rootpath_test.go
+++ internal/rootpath/rootpath_test.go
@@ -1,11 +1,32 @@
 package rootpath_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/Arm-Debug/remoteproc-runtime/internal/rootpath"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestTildeResolve(t *testing.T) {
+	homedir, _ := os.UserHomeDir()
+
+	t.Run("returns absolute homedir path when ~ is provided", func(t *testing.T) {
+		got, _ := rootpath.ExpandTilde("~")
+
+		want := homedir
+
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("returns absolute path/relative path against ~ when ~ is provided", func(t *testing.T) {
+		got, _ := rootpath.ExpandTilde("~/foo/bar")
+
+		want := homedir + "/foo/bar"
+
+		assert.Equal(t, want, got)
+	})
+}
 
 func TestJoin(t *testing.T) {
 	t.Run("returns prefix when no path segments specified", func(t *testing.T) {
