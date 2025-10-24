95f28e43d616511c75333cdc94b3bec7548993d0
Yejin Seo <yejseo01@arm.com>
2025-10-24 11:11:59 +0100

    fix: Define user directories based on the user not environment variables
    conmon sanitizes all environment variables before calling runtimes. The
    only two environment variables that are passed are $PATH and
    $XDG_RUNTIME_DIR if the user is non-root($XDG_RUNTIME_DIR won't be passed
    if the user is root).
    
    It's best to define the user directory by using user.HomeDir rather than
    os.UserHomedir() because os.UserHomeDir() assumes the presence of $HOME
    
    Signed-off-by: Yejin Seo <yejseo01@arm.com>

--- internal/userdirs/userdirs.go
+++ internal/userdirs/userdirs.go
@@ -1,17 +1,18 @@
 package userdirs
 
 import (
+	"fmt"
 	"os"
+	"os/user"
 	"path/filepath"
 )
 
 func joinHomeDir(elem ...string) (string, error) {
-	localDir, err := os.UserHomeDir()
+	user, err := user.Current()
 	if err != nil {
 		return "", err
 	}
-
-	chunks := append([]string{localDir}, elem...)
+	chunks := append([]string{user.HomeDir}, elem...)
 	return filepath.Join(chunks...), nil
 }
 
@@ -19,6 +20,8 @@ func RuntimeDir() (string, error) {
 	xdgRuntimeDir := os.Getenv("XDG_RUNTIME_DIR")
 	if xdgRuntimeDir != "" {
 		return filepath.Join(xdgRuntimeDir, ".remoteproc-runtime"), nil
+	} else {
+		fmt.Println("XDG_RUNTIME_DIR is not set, falling back to home directory")
 	}
 	return joinHomeDir(".remoteproc-runtime") // I'M NOT SURE ABOUT THIS BEING SENSIBLE DEFAULT
 }
