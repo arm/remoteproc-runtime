59edbf38e8379faaa5e61e81edd1e5e38e081c28
Yejin Seo <yejseo01@arm.com>
2025-11-05 16:52:40 +0000

    test: Update proxy process namespacing to test flow as one user
    Signed-off-by: Yejin Seo <yejseo01@arm.com>

--- e2e/runtime_test.go
+++ e2e/runtime_test.go
@@ -204,17 +204,17 @@ func TestRuntime(t *testing.T) {
 				containerName)
 			require.NoError(t, err, "stderr: %s", stderr)
 			t.Cleanup(func() {
-				_, _, _ = installedRuntimeSudo.Run("delete", containerName)
+				_, _, _ = installedRuntime.Run("delete", containerName)
 			})
 
-			pid, err := getContainerPid(installedRuntimeSudo, containerName)
+			pid, err := getContainerPid(installedRuntime, containerName)
 			require.NoError(t, err)
 
 			requireSameMountNamespace(t, vm, uint(pid))
 
 			remoteproc.AssertState(t, sim.DeviceDir(), "offline")
 
-			_, stderr, err = installedRuntimeSudo.Run("start", containerName)
+			_, stderr, err = installedRuntime.Run("start", containerName)
 			require.NoError(t, err, "stderr: %s", stderr)
 			remoteproc.AssertState(t, sim.DeviceDir(), "running")
 		})
@@ -321,6 +321,7 @@ func getContainerPid(runtime limavm.Runnable, containerName string) (int, error)
 
 func getContainerState(runtime limavm.Runnable, containerName string) (specs.State, error) {
 	var state specs.State
+	//this also needs to be non root
 	out, stderr, err := runtime.Run("state", containerName)
 	if err != nil {
 		return state, fmt.Errorf("command failed: %w\n<stderr>\n%s\n</stderr>", err, stderr)
