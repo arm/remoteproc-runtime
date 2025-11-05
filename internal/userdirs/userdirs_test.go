92c6f22187d3040435bd90a08ab7481d18cb3ea1
Yejin Seo <yejseo01@arm.com>
2025-11-05 13:30:56 +0000

    tests: Improve tests to use testify better
    Signed-off-by: Yejin Seo <yejseo01@arm.com>

--- internal/userdirs/userdirs_test.go
+++ internal/userdirs/userdirs_test.go
@@ -1,7 +1,7 @@
 package userdirs_test
 
 import (
-	"os"
+	"os/user"
 	"path/filepath"
 	"testing"
 
@@ -14,25 +14,21 @@ func TestRuntimeDir(t *testing.T) {
 		testDir := "/tmp/xdg_runtime_test"
 		t.Setenv("XDG_RUNTIME_DIR", testDir)
 		want := filepath.Join(testDir, ".remoteproc-runtime")
+
 		got, err := userdirs.RuntimeDir()
+
 		require.NoError(t, err)
 		require.Equal(t, want, got)
 	})
 	t.Run("when XDG_RUNTIME_DIR is not set, defaults to $HOME/.remoteproc-runtime", func(t *testing.T) {
-		xdgRuntimeDirOriginal := os.Getenv("XDG_RUNTIME_DIR")
-		if xdgRuntimeDirOriginal != "" {
-			err := os.Unsetenv("XDG_RUNTIME_DIR")
-			require.NoError(t, err)
-		}
-		defer func() {
-			err := os.Setenv("XDG_RUNTIME_DIR", xdgRuntimeDirOriginal)
-			require.NoError(t, err)
-		}()
-		home, err := os.UserHomeDir()
+		t.Setenv("XDG_RUNTIME_DIR", "")
+		user, err := user.Current()
+		home := user.HomeDir
 		require.NoError(t, err)
-
 		want := filepath.Join(home, ".remoteproc-runtime")
+
 		got, err := userdirs.RuntimeDir()
+
 		require.NoError(t, err)
 		require.Equal(t, want, got)
 	})
