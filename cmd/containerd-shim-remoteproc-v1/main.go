e5cef5549987c1f3b4420058683709223df28d09
Bartek Mucha <bartosz.mucha@arm.com>
2026-07-08 16:00:35 +0100

    Make shim compatible with 2.2.x and 2.3.x
    Signed-off-by: Bartek Mucha <bartosz.mucha@arm.com>

--- cmd/containerd-shim-remoteproc-v1/main.go
+++ cmd/containerd-shim-remoteproc-v1/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/arm/remoteproc-runtime/internal/shim"
 	containerdshim "github.com/containerd/containerd/v2/pkg/shim"
 )
 
 func main() {
+	ctx := context.Background()
 	manager := shim.NewManager("io.containerd.remoteproc.v1")
-	containerdshim.RunShim(context.Background(), manager)
+	if handled, err := runStartCompat(ctx, manager); handled {
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s", manager.Name(), err)
+			os.Exit(1)
+		}
+		return
+	}
+	containerdshim.RunShim(ctx, manager)
 }
